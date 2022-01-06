0e10063594672359e3567c3123774600b9b087ea
Yannick Debrée <contact@yannickdebree.com>
2022-01-06 17:16:46 +0100

    fix: doc folder renamed

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import * as path from 'path';
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "./doc", resolve: {
+    root: "./docs-src", resolve: {
         alias: [
             { find: '@ouikit', replacement: path.resolve(__dirname, 'packages') }
         ]
     },
     build: {
-      outDir: "../doc-dist",
+        outDir: "../docs",
       emptyOutDir: true,
     },
 });
